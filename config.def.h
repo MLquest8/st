88c868bb5c0a3575e58d6e12f8887e6ac2d496f5
MLquest8 <miskuzius@gmail.com>
2020-12-05 23:17:27 +0400

    shortcut change

--- config.def.h
+++ config.def.h
@@ -217,6 +217,7 @@ static MouseShortcut mshortcuts[] = {
 /* Internal keyboard shortcuts. */
 #define MODKEY Mod1Mask
 #define TERMMOD (ControlMask|ShiftMask)
+#define TERMMOD2 (ControlMask|Mod1Mask)
 
 static Shortcut shortcuts[] = {
 	/* mask                 keysym          function        argument */
@@ -232,7 +233,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ MODKEY,               XK_c,           normalMode,     {.i =  0} },
+	{ TERMMOD2,             XK_c,           normalMode,     {.i =  0} },
 };
 
 /*
